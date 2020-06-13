b277a2ed69880258693c35d04da64c5473e66d7e
simonsanchezart <simonsanchez.art@gmail.com>
2020-06-13 17:46:53 -0300

    Youtube searcher

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import requests
-from sys import exit
 import json
-
-def check_quit(input_text):
-    if input_text is "q":
-        exit()
+import os
 
 def check_confirmation(input_text):
     available_anwers = ("yes", "y", "yey")
@@ -16,12 +12,10 @@ def check_confirmation(input_text):
 
 def is_strict():
     strict_confirmation = input("Do you want to perform a strict search? (y/n) ")
-    check_quit(strict_confirmation)
     return check_confirmation(strict_confirmation)
 
 
 search_term = input("What do you wanna search for? ")
-check_quit(search_term)
 
 wiki_articles, wiki_in_links = [], []
 def search_wikipedia():
@@ -58,18 +52,81 @@ def search_wikipedia():
             wiki_articles.append(full_article)
 
 
-search_wikipedia()
+wiki_confirm = check_confirmation(input("Do you want to search the Wikipedia? (y/n) "))
+if wiki_confirm:
+    search_wikipedia()
+
+youtube_videos, youtube_thumbnails = [], []
+def youtube_search(api_key):
+    global search_term
+
+    if is_strict():
+        url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=\"{search_term}\"&relevanceLanguage=en&type=video&key={api_key}"
+    else:
+        url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q={search_term}&relevanceLanguage=en&type=video&key={api_key}"
+
+    r = requests.get(url)
+    contents = r.json()
+    print(f"Youtube connection: {r.status_code}")
+    if r.status_code != 200:
+        print("Not able to use connect to Youtube\n")
+    else:
+        with open("google_api.txt", "w") as f:
+            f.write(api_key)
+        videos = contents["items"]
+        for video in videos:
+            name = video["snippet"]["title"]
+            id = video["id"]["videoId"]
+            video_link = f"<b><a href='https://www.youtube.com/watch?v={id}' target='_blank'>{name}</a></b>"
+            thumbnail_image = f"""<img src='{video["snippet"]["thumbnails"]["high"]["url"]}'></img>'"""
+            youtube_thumbnails.append(thumbnail_image)
+            youtube_videos.append(video_link)
+
+
+youtube_confirm = check_confirmation(input("Do you want to search for Youtube? (y/n) "))
+if youtube_confirm:
+    filename = "google_api.txt"
+    use_prev = False
+    if os.path.isfile(filename):
+        with open(filename, "r") as f:
+            prev_api = f.read()
+        use_prev = check_confirmation(input(f"A previous API key ({prev_api}) "
+                                            f"has been detected, do you want to use this key? (y/n) "))
+
+    if use_prev:
+        youtube_search(prev_api)
+    else:
+        print(f"\nTo use the Youtube and google services you need an API key.")
+        has_api = check_confirmation(input("Do you have an API key? (y/n) "))
+        if has_api:
+            api_key = input("Input your API key: ")
+            youtube_search(api_key)
+        else:
+            print("You can follow these steps to get your API key: "
+                  "https://developers.google.com/youtube/v3/getting-started"
+                  " Youtube and Google services won't be used.")
+
+if youtube_confirm:
+    write_youtube_thumbnails = check_confirmation(input("Do you want to include Youtube thumbnail? (y/n) "))
 
 def write_file():
     file_name = f"information_about_{search_term}.html"
     with open(file_name, "w", encoding='utf-8-sig', ) as f:
-        f.write(f"<h2>Wikipedia</h2>")
-        for article in enumerate(wiki_articles):
-            f.write(f"{article[1]}")
-            f.write(f"<ul>")
-            for link in wiki_in_links[article[0]]:
-                f.write(f"<li>{link}</li>")
-            f.write(f"</ul><br >")
+        if wiki_confirm:
+            f.write(f"<h2>Wikipedia</h2>")
+            for article in enumerate(wiki_articles):
+                f.write(f"{article[1]}")
+                f.write(f"<ul>")
+                for link in wiki_in_links[article[0]]:
+                    f.write(f"<li>{link}</li>")
+                f.write(f"</ul><br >")
+
+        if youtube_confirm:
+            f.write(f"<h2>Youtube</h2>")
+            for video in range(len(youtube_videos)):
+                f.write(f"{youtube_videos[video]}<br >")
+                if write_youtube_thumbnails:
+                    f.write(f"{youtube_thumbnails[video]}<br >")
 
 
 write_file()
